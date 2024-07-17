b69c6fb1201df362907d1e6c9f1bfcf868c0bd5a
PingEn412 <168167366+PingEn412@users.noreply.github.com>
2024-07-17 17:21:44 +0800

    Add files via upload

---
+++ test_demo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 17:17:44 2024
+
+@author: e55677
+"""
+
+def test_export_report_1():
+    a = 1+1
+    b = 2+2
+    
+    assert b > a
+def test_export_report_2():
+    a = 2+2
+    b = 4+4
+    
+    assert a < b
